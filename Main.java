99c96eddaafaed98564dc00d5392b06fd172c341
RafaelCGConrado <106274045+RafaelCGConrado@users.noreply.github.com>
2023-06-25 19:50:53 -0300

    fim da versão com laço

--- Main.java
+++ Main.java
@@ -28,6 +28,47 @@ public class Main{
             + Integer.toString(this._active));
         }
     }
+
+    public static Country[] sort(Country[] arr, int size, String type){
+
+        for (int i = 1; i < size; i++){
+            int j = i-1;
+            
+        
+            switch(type){
+                case "Country":
+                    while (j >= 0 && arr[j]._country.compareTo(arr[j+1]._country) > 0){
+                        Country temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                        j -= 1;
+                    }
+                    break;
+
+                case "Confirmed":
+                    while (j >= 0 && arr[j + 1]._confirmed < arr[j]._confirmed){
+                        Country temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                        j -= 1;
+                    }
+                    break;
+                    
+                case "Active":
+                    while (j >= 0 && arr[j + 1]._active < arr[j]._active){
+                        Country temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                        j -= 1;
+                    }
+                
+                    break;
+            }
+            
+        }
+        // for(int i = 0; i < size; i++) arr[i].print();
+        return arr;
+    }
     
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
@@ -54,33 +95,42 @@ public class Main{
             Scanner le_arquivo = new Scanner(arq);
             int soma = 0;
             
+            int activeCount = 0;
             Country[] maioresActive = new Country[n[1]];
-            maioresActive[0]._active = -1;
-            maioresActive[0]._death = -1;
+            maioresActive[0] = new Country("NULO,-1,-1,-1,-1");
             
+            int confirmedCount = 0;
             Country[] maioresConfirmed = new Country[n[3]];
-            maioresConfirmed[0]._confirmed = -1;
+            maioresConfirmed[0] = new Country("NULO,-1,-1,-1,-1");
 
             while(le_arquivo.hasNextLine()){
                 String linha = le_arquivo.nextLine();
                 Country c = new Country(linha);
-                c.print();
+                // c.print();
 
                 //1)
                 if(c._confirmed >= n[0]) soma += c._active;
                 
                 //2)
-                if (maioresActive.length < n[1]){
+                if (activeCount < n[1]){
                     //escreve na primeira posicao livre
+                    maioresActive[activeCount] = c;
+                    activeCount++;
+
                 }
                 else if (c._active > maioresActive[0]._active){
                     //escrever na primeira posicao
                     maioresActive[0] = c;
+                    
+
+
                 }
 
                 //3)
-                if (maioresConfirmed.length < n[3]) {
+                if (confirmedCount < n[3]) {
                     //escreve na primeira posicao livre
+                    maioresConfirmed[confirmedCount] = c;
+                    confirmedCount++;
                 }
                 else if (c._confirmed > maioresConfirmed[0]._confirmed) {
                     //escrever na primeira posicao
@@ -88,10 +138,15 @@ public class Main{
                 }
                 
                 // Sort maioresActive por "Active" ...
+                maioresActive = sort(maioresActive, activeCount, "Active");
+                maioresConfirmed = sort(maioresConfirmed, confirmedCount, "Confirmed");
+                
 
                 // Sort maioresConfirmed por "Confirmed" ...
                 
             }
+
+            // for(int i = 0; i < n[1]; i++) maioresActive[i].print();
             // 1000000 10 4 2
             // 32454556
             System.out.println(soma);
@@ -99,10 +154,14 @@ public class Main{
             // 2)
             // Sort maioresActive por "Confirmed" ...
             // Printar os "death" dos n3 países com menor confirmed ...
+            maioresActive = sort(maioresActive, n[1], "Confirmed");
+            for(int i = 0; i < n[2]; i++) System.out.println(maioresActive[i]._death);
 
             // 3)
             // Sort maioresConfirmd por "country" ...
             // Printar maioresConfirmed ...
+            maioresConfirmed = sort(maioresConfirmed, n[3], "Country");
+            for(int i = 0; i < n[3]; i++) maioresConfirmed[i].print();
 
             le_arquivo.close();
         }
